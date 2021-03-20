fc701b152043a10b71513c2ad73ebb905b4800a2
mehdihasan <email2mehdihasan@gmail.com>
2021-03-20 15:34:35 +0600

    pandas basics

---
+++ data-analysis-with-python/datasource/basics.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+##
+## Reading a CSV file using Pandas
+##
+
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/autos/imports-85.data"
+df = pd.read_csv(url, header=None)
+
+
+##
+## Replace default headers
+##
+
+headers = ["symboling", "normalized-losses", "make", "fuel-type", "aspiration", "num-of-doors", "body-style", "drive-wheels", "engine-location", "wheel-base", "length", "width", "height", "curb-weight", "engine-type", "num-of-cylinders", "engine-size", "fuel-system", "bore", "stroke", "compression-ratio", "horsepower", "peak-rpm", "city-mpg", "highway-mpg", "price"]
+df.columns = headers
+
+
+##
+## Printing part of dataß
+##
+
+print(df.head(5))
+print(df.tail(3))
+
+##
+## Exporting a Pandas dataframe to CSV
+##
+
+path = "results/test_result.csv"
+df.to_csv(path)
